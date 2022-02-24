cc0819512ef2c62bb92a627d2d4b8a69268ea5a4
Tomáš Kryčfalušij <91247418+TomasKrycfalusij@users.noreply.github.com>
2022-02-24 09:07:05 +0100

    Počáteční soubory projektu MakeCode

---
+++ main.py
@@ -0,0 +1,80 @@
+pins.touch_set_mode(TouchTarget.P1, TouchTargetMode.CAPACITIVE)
+pins.touch_set_mode(TouchTarget.P2, TouchTargetMode.CAPACITIVE)
+
+is_pin1 = input.pin_is_pressed(TouchPin.P1)
+is_pin2 = input.pin_is_pressed(TouchPin.P2)
+
+player1 = False
+player2 = False
+playing = False
+ready = True
+clicked_early1 = False
+clicked_early2 = False
+
+input.on_pin_pressed(TouchPin.P1, player1_pressed)
+input.on_pin_pressed(TouchPin.P2, player2_pressed)
+
+def player1_pressed():
+    global player1
+    clicked_early1 = True
+    player1 = True
+
+def player2_pressed():
+    global player2
+    clicked_early2 = True
+    player2 = True
+
+
+def start():
+    while ready == True:
+        global ready, playing
+        basic.clear_screen()
+        basic.pause(randint(1000, 3000))
+        playing = True
+        music.play_melody("D", 500)
+
+        while playing == True and player1 == False and player2 == False:
+            basic.show_leds("""
+            . # # # .
+            # . . . #
+            # . # . #
+            # . . . #
+            . # # # .
+            """)
+        basic.pause(3000)
+
+forever(start)
+
+
+def vyhodnocovani():
+    global playing, player1, player2
+    if playing == True:
+        if player1 == True:
+            basic.show_string("1")
+            restart()
+        elif player2 == True:
+            basic.show_string("2")
+            restart()
+    elif playing == False:
+        if player1 == True:
+            playing = False
+            basic.show_string("A")
+            restart()
+        elif player2 == True:
+            playing = False
+            basic.show_string("B")
+            restart()
+forever(vyhodnocovani)
+
+
+def restart():
+    global player1, player2, playing, ready, clicked_early1, clicked_early2
+    player1 = False
+    player2 = False
+    ready = False
+    clicked_early1 = False
+    clicked_early2 = False
+    playing = False
+    basic.clear_screen()
+    basic.pause(3000)
+    ready = True
---
+++ main.ts
@@ -0,0 +1,77 @@
+pins.touchSetMode(TouchTarget.P1, TouchTargetMode.Capacitive)
+pins.touchSetMode(TouchTarget.P2, TouchTargetMode.Capacitive)
+let is_pin1 = input.pinIsPressed(TouchPin.P1)
+let is_pin2 = input.pinIsPressed(TouchPin.P2)
+let player1 = false
+let player2 = false
+let playing = false
+let ready = true
+let clicked_early1 = false
+let clicked_early2 = false
+input.onPinPressed(TouchPin.P1, function player1_pressed() {
+    
+    let clicked_early1 = true
+    player1 = true
+})
+input.onPinPressed(TouchPin.P2, function player2_pressed() {
+    
+    let clicked_early2 = true
+    player2 = true
+})
+forever(function start() {
+    while (ready == true) {
+        
+        basic.clearScreen()
+        basic.pause(randint(1000, 3000))
+        playing = true
+        music.playMelody("D", 500)
+        while (playing == true && player1 == false && player2 == false) {
+            basic.showLeds(`
+            . # # # .
+            # . . . #
+            # . # . #
+            # . . . #
+            . # # # .
+            `)
+        }
+        basic.pause(3000)
+    }
+})
+forever(function vyhodnocovani() {
+    
+    if (playing == true) {
+        if (player1 == true) {
+            basic.showString("1")
+            restart()
+        } else if (player2 == true) {
+            basic.showString("2")
+            restart()
+        }
+        
+    } else if (playing == false) {
+        if (player1 == true) {
+            playing = false
+            basic.showString("A")
+            restart()
+        } else if (player2 == true) {
+            playing = false
+            basic.showString("B")
+            restart()
+        }
+        
+    }
+    
+})
+function restart() {
+    
+    player1 = false
+    player2 = false
+    ready = false
+    clicked_early1 = false
+    clicked_early2 = false
+    playing = false
+    basic.clearScreen()
+    basic.pause(3000)
+    ready = true
+}
+
